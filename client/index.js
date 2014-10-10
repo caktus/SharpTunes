aadfa6318353e91db3e2d784ff3c5b2ba28c896e
Calvin Spealman <calvin@caktusgroup.com>
2014-10-10 00:59:59 -0400

    Added copyright notice on pages. Removed Web Audio initialization that never got used

--- client/index.js
+++ client/index.js
@@ -1,21 +1,3 @@
-var concat = require('concat-stream')
-var prettysize = require('prettysize')
-
 var Playlist = require('./playlist')
 var Player = require('./player')
 var Peers = require('./peers')
-
-// Audio
-
-window.context = null;
-window.addEventListener('load', init, false);
-function init() {
-    try {
-      // Fix up for prefixing
-      window.AudioContext = window.AudioContext||window.webkitAudioContext;
-      window.context = new AudioContext();
-    }
-    catch(e) {
-        alert('Web Audio API is not supported in this browser');
-    }
-}
